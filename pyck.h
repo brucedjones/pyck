72bce4dc758304332da0f7f0a531c53785a5e1dd
Bruce Jones <bdjones@mit.edu>
2016-04-04 12:39:34 -0400

    + Working C++ code for FCC packing

--- pyck.h
+++ pyck.h
@@ -1,4 +1,13 @@
 #ifndef PYCK_H
 #define PYCK_H
 
+void Init();
+void TearDown();
+long ID(long i, long j, long k);
+long DimID(long thisDim,long i, long j, long k);
+void IDX2Pos(long i, long j, long k, float *posOut);
+void PackCube(int p1[3], int p2[3]);
+void WriteCSV();
+
+
 #endif
