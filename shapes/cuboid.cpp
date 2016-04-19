eb3f8b36197bd95daacd221ea66d475450b407ce
Bruce Jones <bdjones@mit.edu>
2016-04-19 17:13:24 -0400

    + Bug fixes

--- shapes/cuboid.cpp
+++ shapes/cuboid.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
+
 #include "cuboid.h"
 
 Cuboid::Cuboid(int state, double *p1, double *p2) : Shape(state)
 {
   this->p1 = new double[3];
   this->p2 = new double[3];
-  std::copy(p1, p1+3, this->p1);
-  std::copy(p2, p2+3, this->p2);
+  this->p1[0] = p1[0]; this->p1[1] = p1[1]; this->p1[2] = p1[2];
+  this->p2[0] = p2[0]; this->p2[1] = p2[1]; this->p2[2] = p2[2];
 
-  this->boundingBox = new BoundingBox(p1,p2);
+  this->boundingBox = new BoundingBox(this->p1,this->p2);
 }
 
-Cuboid::~Cuboid(){}
+Cuboid::~Cuboid(){
+  delete [] this->p1;
+  delete [] this->p2;
+}
 
 bool Cuboid::IsInside(double *pt)
 {
