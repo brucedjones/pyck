f1deb60cd4c716e6a785ab6dbdeda7523287d65e
Bruce Jones <bdjones@mit.edu>
2016-04-25 17:13:31 -0400

    + Fixed a bug

--- boundingBox.cpp
+++ boundingBox.cpp
@@ -12,8 +12,15 @@ BoundingBox::BoundingBox(double *p1, double *p2)
     }
   }
 
-  this->p1 = p1;
-  this->p2 = p2;
+  this->p1 = new double[3];
+  this->p1[0] = p1[0];
+  this->p1[1] = p1[1];
+  this->p1[2] = p1[2];
+
+  this->p2 = new double[3];
+  this->p2[0] = p2[0];
+  this->p2[1] = p2[1];
+  this->p2[2] = p2[2];
 }
 
 BoundingBox::~BoundingBox()
